0763782edfe9d5a5c0b9d5468f4e5038f64ccd09
Tiến <letien3413@gmail.com>
2024-10-21 19:17:20 +0700

    removing cities from localstorage

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -45,13 +45,21 @@
     <hr class="border-white border-opacity-10 border w-full" />
     <!-- widgets weather -->
     <div id="openweathermap-widget-1" class="mt-8"></div>
+    <div
+      class="flex items-center gap-2 py-12 text-white cursor-pointer duration-150 hover:text-red-500"
+      @click="removeCity"
+      v-if="route.query.id"
+    >
+      <i class="fa-solid fa-trash"></i>
+      <p>Remove City</p>
+    </div>
   </div>
 </template>
 
 <script setup>
 import axios from "axios";
 import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { watch } from "vue";
 
 const unit = ref("metric");
@@ -103,6 +111,16 @@ onMounted(() => {
     console.error("Lỗi.");
   }
 });
+// remove city from localStorage
+const router = useRouter();
+const removeCity = () => {
+  const cities = JSON.parse(localStorage.getItem("savedCities"));
+  const updatedCities = cities.filter((city) => city.id !== route.query.id);
+  localStorage.setItem("savedCities", JSON.stringify(updatedCities));
+  router.push({
+    name: "home",
+  });
+};
 </script>
 
 <style lang="scss" scoped></style>
